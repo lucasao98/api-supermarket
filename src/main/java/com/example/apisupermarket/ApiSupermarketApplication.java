41f02a5dece23446aa8a0bb2cc37610d998d39df
lucasao98 <lucasacramento32@gmail.com>
2024-03-17 16:44:35 -0300

    Initial Commit

---
+++ src/main/java/com/example/apisupermarket/ApiSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apisupermarket/ApiSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apisupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
